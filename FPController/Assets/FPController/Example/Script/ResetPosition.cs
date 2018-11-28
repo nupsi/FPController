e91b5811f8d7b3e0940c8463cce04463dbf20212
Nupsi <nupsi4.thenoob@gmail.com>
2018-11-28 04:21:53 +0200

    Added simple reset position script to example scene.

---
+++ FPController/Assets/FPController/Example/Script/ResetPosition.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace FPController.Example
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class ResetPosition : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject m_resetPosition;
+
+        private void Reset()
+        {
+            var collider = GetComponent<BoxCollider>();
+            collider.isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "Player")
+            {
+                other.gameObject.transform.position = Position;
+                var rididbody = other.gameObject.GetComponent<Rigidbody>();
+                if(rididbody != null)
+                {
+                    rididbody.velocity = Vector3.zero;
+                }
+            }
+        }
+
+        private Vector3 Position
+        {
+            get
+            {
+                return m_resetPosition != null
+                    ? m_resetPosition.transform.position
+                    : Vector3.up;
+            }
+        }
+    }
+}
