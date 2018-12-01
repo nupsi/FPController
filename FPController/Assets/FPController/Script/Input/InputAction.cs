4dd84e0080e34f24c227f6413dc8d7264686b5d1
Nupsi <nupsi4.thenoob@gmail.com>
2018-12-01 09:30:43 +0200

    Added new constructor for input action. Also added example for usage.

--- FPController/Assets/FPController/Script/Input/InputAction.cs
+++ FPController/Assets/FPController/Script/Input/InputAction.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 namespace FPController
 {
     /// <summary>
-    /// Input Action to store two actions to be called on KeyDown() and KeyUp() when KeyCode is Up/Down.
-    /// <see cref="KeyDown"/>
-    /// <see cref="KeyUp"/>
-    /// <see cref="KeyCode"/>
+    /// Stores key code(s) and reference(s) to function(s).
+    /// Allows simple way to check if key is used and call stored function.
+    /// <example>
+    /// //Define array of differend action.
+    /// var actions = InputAction[] 
+    /// { 
+    ///     //Create input action for jump.
+    ///     //controller.Jump() gets called when actions[0].KeyDown() is called.
+    ///     new InputAction(KeyCode.Space, controller.Jump, null)
+    /// };
+    /// ...
+    /// //Loop through the array of actions.
+    /// foreach(var action in actions)
+    /// {
+    ///     //Check if key is down for current action.
+    ///     if(Input.GetKeyDown(action.KeyCode))
+    ///     {
+    ///         //Call action.KeyDown() to execute function referenced by input action.
+    ///         //For this example this would call controller.Jump() when space is pressed.
+    ///         action.KeyDown();
+    ///     }
+    ///     //Check if pressed and up.
+    /// }
+    /// </example>
     /// </summary>
     public class InputAction
     {
@@ -24,26 +44,24 @@ namespace FPController
          * Public Functions.
          */
 
-        public InputAction(KeyCode _key, Action _downEvent, Action _upEvent) : this(_key, _downEvent, _upEvent, KeyCode.None)
-        {
-        }
+        public InputAction(KeyCode _key, Action _downEvent, Action _upEvent) 
+            : this(_key, _downEvent, _upEvent, KeyCode.None) { }
 
-        public InputAction(KeyCode _key, Action _downEvent, Action _upEvent, KeyCode _combination)
-        {
-            KeyCode = _key;
-            m_keyDownEvent = _downEvent;
-            m_keyUpEvent = _upEvent;
-            m_combinationKey = _combination;
-        }
+        public InputAction(KeyCode _key, Action _downEvent, Action _upEvent, KeyCode _combination) 
+            : this(_key, _downEvent, null, _upEvent, _combination) { }
 
-        public InputAction(KeyCode _key, Action _event) : this(_key, _event, KeyCode.None)
-        {
-        }
+        public InputAction(KeyCode _key, Action _event) 
+            : this(_key, _event, KeyCode.None) { }
 
-        public InputAction(KeyCode _key, Action _event, KeyCode _combination)
+        public InputAction(KeyCode _key, Action _event, KeyCode _combination) 
+            : this(_key, null, _event, null, _combination) { }
+
+        public InputAction(KeyCode _key, Action _downEvent, Action _event, Action _upEvent, KeyCode _combination)
         {
             KeyCode = _key;
             m_keyEvent = _event;
+            m_keyUpEvent = _upEvent;
+            m_keyDownEvent = _downEvent;
             m_combinationKey = _combination;
         }
 
